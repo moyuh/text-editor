5dc8bd4033c2c0181f2a5aa83f48bd623733e6d2
moyuh <mlroth94@gmail..com>
2022-11-07 11:12:29 -0700

    hopefully fixes get issues

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,8 +35,8 @@ module.exports = () => {
         ios: true,
         background_color: "#225ca3",
         theme_color: '#225ca3',
-        start_url: '/',
-        publicPath: '/',
+        start_url: './',
+        publicPath: './',
         icons: [{
           src: path.resolve('src/images/logo.png'),
           sizes: [96, 128, 192, 256, 384, 512],
